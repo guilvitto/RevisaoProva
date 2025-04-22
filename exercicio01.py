23e7449b37e491fbceb6ecd98ba5209090a9601b
Guilherme Vitório <gui.vitoriot@gmail.com>
2025-04-22 08:34:34 -0300

    Enviando exercicio

---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+a = float(input("Digite o valor de A: "))
+b = float(input("Digite o valor de B: "))
+c = float(input("Digite o valor de C: "))
+soma=a+b
+print(soma)
+if soma<c:
+    print(f"Soma = {soma}é menor que {c} !")
